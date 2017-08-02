282605dbb10f3aa9550c93442fa295367c533fbd
sahil <sahil9619299166@gmail.com>
2017-08-02 20:23:33 +0530

    Added port scanning feature for Our tool , released version 1.1 !!!

--- ipscan.sh
+++ ipscan.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-echo "------------------- SCAN DEVICES IN YOUR NETOWRK -------------------"
-echo "                      IP scanner v1.0 @tikoo                        "
+echo "------------------- SCAN DEVICES IN YOUR NETWORK -------------------"
+echo "                    PingPort Network Scanner v1.1 @tikoo                        "
 if [ "$1" == "" ]
 then
-echo "Example: ./ipscan.sh networkid"
-else
+echo "IpScan: ./ipscan.sh networkid"
+echo "PortScan: ./ipscan.sh networkid port"
+elif [ "$2" != "" ]
+then
 x=1
-echo "Active hosts shown below| Log Generated as well "
+echo "Active hosts shown below with Port Scan| Portlog Generated"
+while [ $x -lt 256 ] ; do
+nc -zv $1.$x $2 2>&1 | grep "open"| cut -d " " -f 2,3,4| ts '[%Y-%m-%d %H:%M:%S]'| tee -a portlog.txt  
+((x++))
+done
+else
+echo "Active hosts shown below| PingLog Generated "
+x=1
 while [ $x -lt 256 ] ; do
 ping -c 1 $1.$x | grep "64 bytes" | cut -d " " -f 4 | sed s'/.$//'| ts '[%Y-%m-%d %H:%M:%S]' | tee -a pinglog.txt
 (( x++ ))
